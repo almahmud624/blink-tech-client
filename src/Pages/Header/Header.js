4cacd89d848ec3f431c7827bc938fb2c0d6bb986
almahmud624 <mahmudbhuiyan624@gmail.com>
2022-10-31 01:35:01 +0600

    initial setup complete

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Shared/Navbar";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar />
+      <h3>this is header</h3>
+    </div>
+  );
+};
+
+export default Header;
